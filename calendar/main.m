0a58d75120947f24b2b88fa4a51ffe456177eac7
7Charlotte <liuxia@liuxiatekiMacBook-Air.local>
2013-10-04 09:29:09 +0800

    Initial Commit

---
+++ calendar/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  calendar
+//
+//  Created by Charlotte on 13年10月4日.
+//  Copyright (c) 2013年 Charlotte. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
